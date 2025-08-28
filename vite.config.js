f31ea18e204dfde8728cae1f8ab4a152c3bc6d1b
Hasib Iqbal <hasibiqbal207@gmail.com>
2025-08-28 05:32:33 +0200

    changed the port to 6000

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 6002,
+    port: 6000,
     host: '0.0.0.0',
   },
   base: '/',
